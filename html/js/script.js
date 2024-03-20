48fd0674f64e1aa8221f7344750962caafc202b7
Mostasim Mahmud Tasim <66787305+tasim313@users.noreply.github.com>
2024-03-20 09:44:22 +0600

    Add files via upload
    Create Html Css For Voice Speech To Text and Shortcut Suggestion For Description

---
+++ html/js/script.js
@@ -0,0 +1,17 @@
+document.getElementById('click_to_convert').addEventListener(
+    'click',  function(){
+        var speech = true;
+        window.SpeechRecognition = window.webkitSpeechRecognition;
+        const recognition = new SpeechRecognition();
+        recognition.interimResults = true;
+        
+        recognition.addEventListener('result', e=>{
+            const transcript = Array.from(e.results).map(result => result[0]).map(result => result.transcript)
+            document.getElementById('convert_text').innerHTML = transcript;
+        })
+
+        if(speech == true){
+            recognition.start();
+        }
+    }
+);
